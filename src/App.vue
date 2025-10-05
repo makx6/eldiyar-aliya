849442f856d72e41c078fbd0a2c278cd3919d247
maksat.zairbekov <maksat.zairbekov@gmail.com>
2025-10-05 20:54:33 +0600

    1

--- src/App.vue
+++ src/App.vue
@@ -489,7 +489,7 @@ onBeforeUnmount(() => {
   width: 100%;
   min-height: 100vh;
 
-  background-image: url("./assets/2.jpg");
+  background-image: url("./assets/4.jpg");
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
